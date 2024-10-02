47c0f646ae36ec2afe855bfc7d31adc90dfb614e
Nicolas Arevalo <gabrielnicolas512@outlook.com>
2024-10-02 19:48:12 -0300

    first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,17 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const cardValues = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+  const cardIcons = ["♦", "♥", "♠", "♣"];
+
+  const getRandom = array => array[Math.floor(Math.random() * array.length)];
+  let selectedIcon = getRandom(cardIcons);
+  if (selectedIcon == "♦" || selectedIcon == "♥") {
+    document.querySelector("#topIcon").classList.add("text-danger");
+    document.querySelector("#bottomIcon").classList.add("text-danger");
+  }
+
+  document.querySelector("#topIcon").innerHTML = selectedIcon;
+  document.querySelector("#cardNumber").innerHTML = getRandom(cardValues);
+  document.querySelector("#bottomIcon").innerHTML = selectedIcon;
 };
